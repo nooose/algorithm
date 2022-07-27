310bb523f7ab62d5e876c5f8dd3ae74d04fccd6a
nooose <hd15807@gmail.com>
2022-07-27 21:21:23 +0900

    #6 2753

---
+++ baekjoon/solution/src/impl/_2753/Main.java
@@ -0,0 +1,16 @@
+package impl._2753;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int year = scanner.nextInt();
+
+        System.out.println(isLeapYear(year) ? 1 : 0);
+    }
+
+    private static boolean isLeapYear(int year) {
+        return year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
+    }
+}
