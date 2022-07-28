91087deb2a6b4f9a9bd257140b85e0092ae442e5
nooose <hd15807@gmail.com>
2022-07-29 00:46:25 +0900

    #7 20053

---
+++ baekjoon/solution/src/impl/_20053/Main.java
@@ -0,0 +1,32 @@
+package impl._20053;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 0; i < t; i++) {
+            int n = Integer.parseInt(scanner.nextLine());
+
+            int min = 1000000;
+            int max = -1000000;
+
+            String[] numbers = scanner.nextLine().split(" ");
+
+            for (String strNumber : numbers) {
+                int number = Integer.parseInt(strNumber);
+
+                if (number < min) {
+                    min = number;
+                }
+
+                if (number > max) {
+                    max = number;
+                }
+            }
+            System.out.println(min + " " + max);
+        }
+    }
+}
