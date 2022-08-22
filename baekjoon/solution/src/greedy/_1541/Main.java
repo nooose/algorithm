a7edaab0c6c7d3af1acfd310e6e2dfeca74e282e
nooose <hd15807@gmail.com>
2022-08-23 00:12:54 +0900

    #13 1541

---
+++ baekjoon/solution/src/greedy/_1541/Main.java
@@ -0,0 +1,32 @@
+package greedy._1541;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static final int INIT_VALUE = -1;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] expressions = br.readLine().split("-");
+
+        int answer = INIT_VALUE;
+        for (String expression : expressions) {
+            int sum = 0;
+
+            for (String value : expression.split("\\+")) {
+                sum += Integer.parseInt(value);
+            }
+
+            if (answer == INIT_VALUE) {
+                answer = sum;
+            } else {
+                answer -= sum;
+            }
+        }
+
+        System.out.println(answer);
+    }
+}
