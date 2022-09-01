630d78fb5f1635c2248735ee82275f426ad0eda4
nooose <hd15807@gmail.com>
2022-09-02 01:03:46 +0900

    \#14 17626

---
+++ baekjoon/solution/src/dp/_17626/Main.java
@@ -0,0 +1,32 @@
+package dp._17626;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] dp = new int[n + 1];
+
+        dp[1] = 1; // 1^2
+        dp[2] = 2; // 1^2 + 1^2
+        dp[3] = 3; // 1^2 + 1^2
+        dp[4] = 1; // 2^2
+
+        int min = 0;
+        for (int i = 5; i <= n; i++) {
+            min = Integer.MAX_VALUE;
+
+            for (int j = 1; j * j <= i; j++) {
+                int tmp = i - j * j;
+                min = Math.min(dp[tmp], min);
+            }
+
+            dp[i] = min + 1;
+        }
+
+        System.out.println(dp[n]);
+    }
+}
