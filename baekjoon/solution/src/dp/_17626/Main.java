34ec91693e49f2217d09f188db93771400e9e4e3
nooose <hd15807@gmail.com>
2022-09-02 01:23:32 +0900

    #14 17626

--- baekjoon/solution/src/dp/_17626/Main.java
+++ baekjoon/solution/src/dp/_17626/Main.java
@@ -8,11 +8,12 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int[] dp = new int[n + 1];
+        int[] dp = new int[50001];
 
+        dp[0] = 0;
         dp[1] = 1; // 1^2
         dp[2] = 2; // 1^2 + 1^2
-        dp[3] = 3; // 1^2 + 1^2
+        dp[3] = 3; // 1^2 + 1^2 + 1^2
         dp[4] = 1; // 2^2
 
         int min = 0;
