146e12ef3874b3f03f5924e8727694ccc4d644ab
nooose <hd15807@gmail.com>
2022-09-04 15:12:53 +0900

    #14 11727

---
+++ baekjoon/solution/src/dp/_11727/Main.java
@@ -0,0 +1,24 @@
+package dp._11727;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        List<Integer> dp = new ArrayList<>();
+        dp.add(1);
+        dp.add(3);
+
+        for (int i = 2; i < n; i++) {
+            dp.add((dp.get(i - 1) + (2 * dp.get(i - 2))) % 10007);
+        }
+
+        System.out.println(dp.get(n - 1));
+    }
+}
