098110d6ffb4910b03d115778ab58105c8e1914d
nooose <hd15807@gmail.com>
2022-09-04 14:00:31 +0900

    #14 1463

---
+++ baekjoon/solution/src/dp/_1463/Main.java
@@ -0,0 +1,32 @@
+package dp._1463;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[] dp = new int[1000001];
+
+        dp[1] = 0;
+        dp[2] = 1;
+        dp[3] = 1;
+
+        for (int i = 4; i < n + 1; i++) {
+            dp[i] = dp[i - 1] + 1;
+
+            if (i % 2 == 0) {
+                dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+            }
+
+            if (i % 3 == 0) {
+                dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+            }
+        }
+
+        System.out.println(dp[n]);
+    }
+}
