9e133f4d7c24902699f97e9e8450a8263d37ab58
nooose <hd15807@gmail.com>
2022-09-04 15:05:25 +0900

    #14 2579

---
+++ baekjoon/solution/src/dp/_2579/Main.java
@@ -0,0 +1,29 @@
+package dp._2579;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[] stairs = new int[301];
+        int[] dp = new int[301];
+
+        for (int i = 1; i < n + 1; i++) {
+            stairs[i] = Integer.parseInt(br.readLine());
+        }
+
+        dp[1] = stairs[1];
+        dp[2] = Math.max(stairs[1] + stairs[2], stairs[2]);
+        dp[3] = Math.max(stairs[1] + stairs[3], stairs[2] + stairs[3]);
+
+        for (int i = 4; i < n + 1; i++) {
+            dp[i] = Math.max(dp[i - 2] + stairs[i], stairs[i - 1] + stairs[i] + dp[i - 3]);
+        }
+
+        System.out.println(dp[n]);
+    }
+}
